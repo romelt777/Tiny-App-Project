c563af3ad779f1c0debba6308e7e1c111cb19f10
Romel Tan <romel.tan777@gmail.com>
2019-01-15 18:31:38 +0000

    first commit to project. Files are set up. express server works simply.

---
+++ express_server.js
@@ -0,0 +1,18 @@
+//importing express js
+var express = require("express");
+
+var app = express();
+var PORT = 8080;
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouse.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
